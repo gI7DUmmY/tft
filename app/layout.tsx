9edd2dee8953fba38fd2f2fc749808e13ac4edd9
Cedric Rollo <cedric.rolllo@iut-tlse3.fr>
2024-05-03 11:09:44 +0200

    🔍️ OpenGraph metadatas

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,22 @@ export const metadata: Metadata = {
   title: 'Toulouse Football de Table',
   description:
     "Toulouse Football de Table : club de babyfoot officiel toulousain. Loisirs, competition, accompagnement, animations, esprit d'equipe pour toutes et tous sont au rendez-vous !",
+  openGraph: {
+    title: 'Toulouse Football de Table',
+    description:
+      "Toulouse Football de Table : club de babyfoot officiel toulousain. Loisirs, competition, accompagnement, animations, esprit d'equipe pour toutes et tous sont au rendez-vous !",
+    url: 'https://toulouseft.fr',
+    siteName: 'toulouseft.fr',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/tft---site.appspot.com/o/tft_officiel.png?alt=media&token=eaabc2b1-206a-4aff-bed4-025ed922cbfe', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'fr',
+    type: 'website',
+  },
   other: {
     'color-scheme': 'dark only',
     'twitter:image':
@@ -43,13 +59,6 @@ export const metadata: Metadata = {
     'twitter:url': 'https://toulouseft.fr/',
     'twitter:title': 'Toulouse Football de Table',
     'twitter:description':
-      "Toulouse Football de Table : club de babyfoot officiel toulousain. Loisirs, competition, accompagnement, animations, esprit d'equipe pour toutes et tous sont au rendez-vous !",
-    'og:url': 'https://toulouseft.fr/',
-    'og:image':
-      'https://firebasestorage.googleapis.com/v0/b/tft---site.appspot.com/o/tft_officiel.png?alt=media&token=eaabc2b1-206a-4aff-bed4-025ed922cbfe',
-    'og:type': 'website',
-    'og:title': 'Toulouse Football de Table',
-    'og:description':
       "Toulouse Football de Table : club de babyfoot officiel toulousain. Loisirs, competition, accompagnement, animations, esprit d'equipe pour toutes et tous sont au rendez-vous !",
   },
 }
