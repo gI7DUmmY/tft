44228598b5b92ceecd9b6c8590f980d1244bc0a3
Cedric Rollo <cedric.rolllo@iut-tlse3.fr>
2024-05-24 15:33:47 +0200

    💄📱 Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 import { MdFacebook, MdLink, MdWhatsapp } from 'react-icons/md'
-import { FaDiscord } from 'react-icons/fa6'
+import { FaDiscord, FaInstagram } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-    <footer className='p-6 pb-2 w-full bg-indigo-900 text-white text-left font-sans xl:p-12 xl:pb-4 2xl:p-16 2xl:pb-6'>
-      <div className='flex flex-col justify-evenly gap-6 md:flex-row lg:flex-none lg:grid lg:grid-cols-3'>
+    <footer className='p-6 pb-2 w-full bg-indigo-900 text-white text-justify font-sans md:text-left md:p-3 xl:p-12 xl:pb-4 2xl:p-16 2xl:pb-6'>
+      <div className='flex flex-col justify-evenly gap-6 md:grid md:grid-cols-2 lg:flex-none lg:grid-cols-4'>
+        {/* Facebook */}
         <div className='shrink-0'>
-          <h3 className='text-2xl pl-1'>
-            <MdFacebook className='inline align-text-bottom text-blue-600' />
+          <h3 className='text-2xl'>
+            <MdFacebook className='inline align-text-bottom text-blue-600 text-3xl' />
             acebook
           </h3>
           <ul>
@@ -32,19 +33,35 @@ const Footer = () => {
             </li>
           </ul>
         </div>
+        {/* Instagram */}
         <div>
-          <h3 className='text-2xl pl-1'>
-            <MdWhatsapp className='inline align-text-bottom mr-1 text-green-600' />
-            <span className='text-xl'>Whatsapp</span>
+          <h3 className='text-2xl'>
+            <FaInstagram className='inline align-text-bottom text-3xl mr-1 bg-gradient-to-br from-indigo-500 via-fuchsia-500 to-rose-400 rounded-md' />
+            Instagram
+          </h3>
+          <Link
+            href='https://www.instagram.com/toulouse_football_de_table/'
+            target='_blank'
+          >
+            <MdLink className='inline mr-1 align-middle' />
+            Découvrez notre galerie!
+          </Link>
+        </div>
+        {/* Whatsapp */}
+        <div>
+          <h3 className='text-2xl'>
+            <MdWhatsapp className='inline align-text-bottom text-3xl mr-1 text-green-600' />
+            Whatsapp
           </h3>
           <p>
             Les annonces internes au club passent par Whatsapp. Demandez
-            &agrave; un responsable du club de vous y inviter!
+            &agrave; un responsable du club de vous y ajouter!
           </p>
         </div>
+        {/* Discord */}
         <div>
-          <h3 className='text-2xl pl-1'>
-            <FaDiscord className='inline align-text-bottom mr-1 text-purple-600' />
+          <h3 className='text-2xl'>
+            <FaDiscord className='inline align-text-bottom mr-1 text-purple-600 text-3xl' />
             Discord
           </h3>
           <p>
